3a512562068385fcf07b201bec85c7595c2cf241
iseyoshitaka <ise0615@gmail.com>
2021-11-20 12:45:01 +0900

    Typescriptの型定義ファイルを追加

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,38 +1,24 @@
-type Props = {
-  id?: string
-  accept?: string
-  capture?: string
-  multiple?: boolean
-  handleChange: (arg0: Return) => void
-  maxFileSize: number
-  thumbnail_size: number
-  drop: boolean
-  dropText: string
-}
-
-type Date = {
-  fileName?: string
-  ofileData?: string
-  fileData?: string
-  ofileSize?: number
-  fileSize?: number
-  fileType?: string
-}
-
-type Result = {
-  result: boolean
-  messages: string[]
-  data: Data
-}
-/**
- * Same as document.activeElement but wraps in a try-catch block. In IE it is
- * not safe to call document.activeElement if there is nothing focused.
- *
- * The activeElement will be null only if the document or document body is not
- * yet defined.
- *
- * @param {?Document} doc Defaults to current document.
- * @return {Element | null}
- * @see https://github.com/facebook/fbjs/blob/master/packages/fbjs/src/core/dom/getActiveElement.js
- */
-export declare function ImageFile(props?: Props): Result
+import { FC } from "react";
+declare type Props = {
+    id?: string;
+    accept?: string;
+    capture?: string;
+    multiple?: boolean;
+    handleChange: (arg0: Return) => void;
+    maxFileSize: number;
+    thumbnail_size: number;
+    drop: boolean;
+    dropText: string;
+};
+declare type Return = {
+    result: boolean;
+    messages: string[];
+    fileName?: string;
+    ofileData?: string;
+    fileData?: string;
+    ofileSize?: number;
+    fileSize?: number;
+    fileType?: string;
+};
+declare const ImageFile: FC<Props>;
+export default ImageFile;
